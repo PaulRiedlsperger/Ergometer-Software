d814ab1bfa383c3ed872fcc72e4c9f3fd51b6536
PaulRiedlsperger <rp0317@mci4me.at>
2025-04-08 13:17:51 +0200

    added superclass

--- my_classes.py
+++ my_classes.py
@@ -1,23 +1,31 @@
 from my_functions import estimate_max_hr
 
-class Subject():
-    def __init__(self, subject):
-        self.subject = subject
-        self.estimate_max_hr = estimate_max_hr(subject['age'], subject['sex'])
-
-        print(self.estimate_max_hr)	
-
-    
-class Supervisor():
-    def __init__(self, supervisor):
-        self.supervisor = supervisor
-    pass    
 
 
-class Experiment():
-    def __init__(self, experiment):
-        self.experiment = experiment 
-    pass         
+class Person():
+    def __init__(self, person_instance :dict):
+        self.first_name = person_instance['first_name']
+        self.last_name = person_instance['last_name']
+        self.age = person_instance['age']
+        self.sex = person_instance['sex']
 
 
-Subject({"id": 1, "first_name": "John", "last_name": "Doe", "sex": "male", "age": 30})
+class Subject(Person):
+    def __init__(self, person_instance :dict):
+        super().__init__(person_instance)
+        self.estimate_max_hr = estimate_max_hr(self.age, self.sex)
+        print(self.estimate_max_hr)
+
+        
+class Supervisor(Person):
+    pass
+
+
+person_instance : dict = {"id": 1, "first_name": "John", "last_name": "Doe", "sex": "male", "age": 30}
+
+# class Experiment():
+#     def __init__(self, experiment):
+#         self.experiment = experiment 
+#     pass         
+
+Subject(person_instance)
